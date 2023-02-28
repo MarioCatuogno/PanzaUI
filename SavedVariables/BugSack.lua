eaebf03dd3128b7a74256cbce76142ca6d3bff2c
Mario Catuogno <mario.catuogno@gmail.com>
2023-02-28 13:49:34 +0100

    Backup SavedVariables

---
+++ SavedVariables/BugSack.lua
@@ -0,0 +1,13 @@
+
+BugSackDB = {
+	["soundMedia"] = "BugSack: Fatality",
+	["altwipe"] = false,
+	["useMaster"] = false,
+	["fontSize"] = "GameFontHighlight",
+	["mute"] = false,
+	["auto"] = false,
+	["chatframe"] = true,
+}
+BugSackLDBIconDB = {
+	["hide"] = false,
+}
