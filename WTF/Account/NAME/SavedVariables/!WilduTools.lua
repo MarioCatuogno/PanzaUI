e9da075a8132b0397efb39b5834693716694d5ee
Mario Catuogno <lister.dollar-0c@icloud.com>
2026-01-26 22:32:48 +0100

    Updated WTF variables for several addons

--- WTF/Account/NAME/SavedVariables/!WilduTools.lua
+++ WTF/Account/NAME/SavedVariables/!WilduTools.lua
@@ -6,29 +6,29 @@ WilduToolsDB = {
 ["Gelatino - Draenor"] = "Default",
 ["Pulcinhell - Draenor"] = "Default",
 ["Panuozzo - Argent Dawn"] = "Default",
-["Sfilatino - Draenor"] = "Default",
+["Giandora - Argent Dawn"] = "Default",
 ["Panzarotto - Draenor"] = "Default",
 ["Percoca - Draenor"] = "Default",
-["Formaggino - Draenor"] = "Default",
 ["Nutellone - Draenor"] = "Default",
-["Giandora - Argent Dawn"] = "Default",
+["Formaggino - Draenor"] = "Default",
+["Sfilatino - Draenor"] = "Default",
 },
 ["profiles"] = {
 ["Default"] = {
 ["editMode"] = {
-["gcdHistory"] = {
-["alpha"] = 1,
-},
-["playerCombat"] = {
-["alpha"] = 1,
-},
 ["rangeCheck"] = {
+["alpha"] = 1,
+},
+["targetCombat"] = {
+["alpha"] = 1,
+},
+["gcdHistory"] = {
 ["alpha"] = 1,
 },
 ["mountIcon"] = {
 ["alpha"] = 1,
 },
-["targetCombat"] = {
+["playerCombat"] = {
 ["alpha"] = 1,
 },
 ["rangeMeleeIndicator"] = {
@@ -37,7 +37,6 @@ WilduToolsDB = {
 },
 ["general_showMinimapButton"] = false,
 ["automation_gossipEnabled"] = true,
-["blizzUI_changeFriendlyNamesFont"] = true,
 ["blizzUI_betterSuperTracked"] = true,
 ["addonsTweaks_bbf_boss_texture_change"] = true,
 ["blizzUI_cleanupObjectiveTracker"] = true,
